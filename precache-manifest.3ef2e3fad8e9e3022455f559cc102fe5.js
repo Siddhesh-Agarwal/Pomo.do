4bd58b5875c9bef9e5845d91764dd95326ca8f5c
Siddhesh Agarwal <siddhesh.agarwal@gmail.com>
2022-11-06 20:48:02 +0530

    Updates

---
+++ precache-manifest.3ef2e3fad8e9e3022455f559cc102fe5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb3534ba7fa10d0862856da1062fb77b",
+    "url": "/pomo.do/index.html"
+  },
+  {
+    "revision": "9becae4a67118b3de6e3",
+    "url": "/pomo.do/static/css/main.c4835697.chunk.css"
+  },
+  {
+    "revision": "02ba8e33adf9ad27e168",
+    "url": "/pomo.do/static/js/2.34205db7.chunk.js"
+  },
+  {
+    "revision": "9becae4a67118b3de6e3",
+    "url": "/pomo.do/static/js/main.5d977e32.chunk.js"
+  },
+  {
+    "revision": "e57ed19a7eb8a878bc3d",
+    "url": "/pomo.do/static/js/runtime~main.c7d71b1a.js"
+  }
+]);
